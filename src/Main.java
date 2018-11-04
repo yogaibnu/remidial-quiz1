24e3e7a46fe0211c9ddd42294cff5f41eb7a20a3
YOGA <yogaibnudinataa@gmail.com>
2018-11-04 14:42:28 +0700

    Menambah try-catch class Main

--- src/Main.java
+++ src/Main.java
@@ -10,23 +10,27 @@
  */
 public class Main {
     public static void main(String[] args) {
-        Penumpang penumpang = new Penumpang();
-        penumpang.setNama("Edward");
-        
-        Jadwal jadwal = new Jadwal();
-        jadwal.setJadwal("10:00 am");
-        jadwal.setTujuan("Surabaya");
-        
-        Pesawat pesawat = new Pesawat();
-        pesawat.setNama("Citilink");
-        pesawat.setJadwal(jadwal);
-        pesawat.setBiaya(450000);
-        pesawat.setJumlah(3);
-        
-        Pembayaran pembayaran = new Pembayaran();
-        pembayaran.setPenumpang(penumpang);
-        pembayaran.setPesawat(pesawat);
-        
-        System.out.println(pembayaran.info());
+        try{
+            Penumpang penumpang = new Penumpang();
+            penumpang.setNama("Edward");
+
+            Jadwal jadwal = new Jadwal();
+            jadwal.setJadwal("10:00 am");
+            jadwal.setTujuan("Surabaya");
+
+            Pesawat pesawat = new Pesawat();
+            pesawat.setNama("Citilink");
+            pesawat.setJadwal(jadwal);
+            pesawat.setBiaya(450000);
+            pesawat.setJumlah(3);
+
+            Pembayaran pembayaran = new Pembayaran();
+            pembayaran.setPenumpang(penumpang);
+            pembayaran.setPesawat(pesawat);
+
+            System.out.println(pembayaran.info());
+        } catch(Exception e){
+            System.out.println();
+        }
     }
 }
