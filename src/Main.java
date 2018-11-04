05ce7215f03db90fcd1d7634fe1eca55345d9b0b
YOGA <yogaibnudinataa@gmail.com>
2018-11-04 14:38:02 +0700

    Membuat intansiasi objek class Main

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,24 @@
  * @author YOGA
  */
 public class Main {
-    
+    public static void main(String[] args) {
+        Penumpang penumpang = new Penumpang();
+        penumpang.setNama("Edward");
+        
+        Jadwal jadwal = new Jadwal();
+        jadwal.setJadwal("10:00 am");
+        jadwal.setTujuan("Surabaya");
+        
+        Pesawat pesawat = new Pesawat();
+        pesawat.setNama("Citilink");
+        pesawat.setJadwal(jadwal);
+        pesawat.setBiaya(450000);
+        pesawat.setJumlah(3);
+        
+        Pembayaran pembayaran = new Pembayaran();
+        pembayaran.setPenumpang(penumpang);
+        pembayaran.setPesawat(pesawat);
+        
+        System.out.println(pembayaran.info());
+    }
 }
